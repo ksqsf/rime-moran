8144940ed42f754faa745bd93e46d6d8ca707ab3
ksqsf <i@ksqsf.moe>
2023-01-31 22:24:02 +0800

    update weights

--- tools/char_freq.py
+++ tools/char_freq.py
@@ -5,23 +5,24 @@
 
 原始字頻數據是簡體字的，故而需要一個辦法調和這個問題。
 
-目前的做法：
+第一步：分配字頻
 
 1. 如果 A 字和 B 字是異體字或簡繁關係，那麼 A 和 B 放在同一個等價類中。
-
-2. 規定：同一個等價類中的字具有相同的字頻，這個等價類的字頻是其中所有字的字頻之和。
-
+2. 規定：同一個等價類中的字具有相同的權重，這個等價類的字頻是其中所有字的字頻之和。
 3. 規定：字頻空間離散化爲 N 等，每個等級具有固定的權重。
-
-4. 將所有等價類根據其字頻分配到這 N 等中。
+4. 將所有等價類根據其字頻分配到這 N 等中，稱爲基準權重 W。
 
 上述分配過程不考慮實際編碼，僅考慮字本身。
 
-(好像並不奏效……？有點奇怪。。。)
+第二步：根據字音相對分配權重。字音相對頻率數據來自於朙月拼音碼表。
+
+設某等價類中一個字具有 n 個讀音，分別是 p[1..n]，頻率分別是 f[1..n]。
+則 p[i] 對應的權重被線性修正爲 W * f[i]。
+如果表中沒有該讀音數據，則賦予權重 0。
 '''
 
-
-from typing import Set, Dict, List
+from zrmify import zrmify
+from typing import Set, Dict, List, Tuple
 import opencc
 
 
@@ -88,6 +89,30 @@ class FrequencyTable:
         return self.table.get(ch, 0)
 
 
+class PronunciationTable:
+    def __init__(self, path: str) -> None:
+        # relative_frequency = table[char][reading]
+        self.table: Dict[Tuple[str,str], float] = dict()
+
+        with open(path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line == '' or line.startswith('#'):
+                    continue
+                [char, reading, *freq] = line.split("\t")
+                if len(freq) == 0:
+                    continue
+                reading = zrmify(reading)
+                freq = freq[0]
+                if freq == '0':
+                    self.table[(char, reading)] = 0.0
+                else:
+                    self.table[(char, reading)] = float(freq[:freq.find('%')]) * 0.01
+
+    def __getitem__(self, pair: Tuple[str,str]) -> float:
+        return self.table.get(pair, 1.0)
+
+
 class CharSet:
     '''記錄字集信息'''
     def __init__(self):
@@ -129,18 +154,20 @@ class CharSet:
             thresholds.append(freqs[n // N_LEVELS * i])
         self.thresholds = thresholds
 
-    def export(self, path: str):
+    def export(self, path: str, pt: PronunciationTable):
         with open(path, 'w') as f:
             for char in self.chars.values():
                 w = level_to_weight[char.level(self.thresholds)]
                 for code in char.codes:
-                    f.write(f'{char.char}\t{code}\t{w}\n')
+                    reading = code.split(';')[0]
+                    f.write(f'{char.char}\t{code}\t{int(w * pt[(char.char, reading)])}\n')
 
 
 if __name__ == '__main__':
     # 字頻數據來自 https://faculty.blcu.edu.cn/xinghb/zh_CN/article/167473/content/1437.htm
     cs = CharSet()
     ft = FrequencyTable('freq.csv')
+    pt = PronunciationTable('luna_pinyin.dict.yaml')
     cs.load_table('zrm.in')
     cs.process_frequency(ft)
-    cs.export('zrm.out')
+    cs.export('zrm.out', pt)
