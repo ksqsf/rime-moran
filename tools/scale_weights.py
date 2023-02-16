8341d5d9003b85ede6472d6f3c01b20b22c2873e
ksqsf <i@ksqsf.moe>
2023-02-16 23:25:24 +0900

    最近的更新

--- tools/scale_weights.py
+++ tools/scale_weights.py
@@ -1,7 +1,8 @@
 # scale weights in [0,1000000] to [0,1]
+# or reverse
 
 import sys
 
 for l in sys.stdin:
     [text, code, weight] = l.split("\t")
-    print(f'{text}\t{code}\t{int(weight) / 1000000:g}')
+    print(f'{text}\t{code}\t{float(weight) * 1000000:g}')
