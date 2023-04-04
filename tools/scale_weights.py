d4dbd6a384aa6e408cd40a31585fb8efa72245a9
ksqsf <i@ksqsf.moe>
2023-04-04 22:04:35 +0800

    例行更新

--- tools/scale_weights.py
+++ tools/scale_weights.py
@@ -5,4 +5,4 @@ import sys
 
 for l in sys.stdin:
     [text, code, weight] = l.split("\t")
-    print(f'{text}\t{code}\t{float(weight) * 1000000:g}')
+    print(f'{text}\t{code}\t{float(weight) / 1000000:g}')
