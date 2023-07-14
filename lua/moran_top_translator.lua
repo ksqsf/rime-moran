c6de0eecba518a64f7c9b9d4c6c660b0664e2bf2
ksqsf <i@ksqsf.moe>
2023-07-14 23:27:43 +0800

    update

--- lua/moran_top_translator.lua
+++ lua/moran_top_translator.lua
@@ -1,16 +1,18 @@
 -- Moran's Top Translator
 -- Copyright (c) 2023 ksqsf
 --
--- Ver: 0.1.0
+-- Ver: 0.2.0
 --
 -- This file is part of Project Moran
 -- Licensed under GPLv3
 --
--- 本翻譯器用於解決 Rime 原生的翻譯流程中，多翻譯器會互相干擾、導致造
--- 詞機能受損的問題。以「驚了」造詞爲例：用戶輸入 jym le，選擇第一個字
--- 「驚」後，再選「了」字，這時候將無法造出「驚了」這個詞。這是因爲
--- 「了」是從碼表翻譯器輸出的，在 script 翻譯器的視角看來，並不知道用
--- 戶輸出了「驚了」兩個字，所以造不出詞。
+-- 0.2.0: 增加固定二字詞模式。
+--
+-- 0.1.0: 本翻譯器用於解決 Rime 原生的翻譯流程中，多翻譯器會互相干擾、
+-- 導致造詞機能受損的問題。以「驚了」造詞爲例：用戶輸入 jym le，選擇第
+-- 一個字「驚」後，再選「了」字，這時候將無法造出「驚了」這個詞。這是
+-- 因爲「了」是從碼表翻譯器輸出的，在 script 翻譯器的視角看來，並不知
+-- 道用戶輸出了「驚了」兩個字，所以造不出詞。
 --
 -- 目前版本的解決方法是：用戶選過字後，臨時禁用 table 翻譯器，使得
 -- script 可以看到所有輸入，從而解決造詞問題。
@@ -55,6 +57,7 @@ function top.func(input, seg, env)
       end
    end
 
+   -- smart 在 fixed 之後輸出。
    local smart_res = env.smart:query(input, seg)
    if smart_res ~= nil then
       for cand in smart_res:iter() do
