be6ecd4a5a7aa1778a5ae466ac1606fcc598b47d
ksqsf <i@ksqsf.moe>
2023-11-26 23:40:48 +0800

    fix(lua): semicolon_processor page

--- lua/moran_semicolon_processor.lua
+++ lua/moran_semicolon_processor.lua
@@ -2,7 +2,10 @@
 -- Synopsis: 選擇第二個首選項，但可用於跳過 emoji 濾鏡產生的候選
 -- Author: ksqsf
 -- License: MIT license
--- Version: 0.1.0
+-- Version: 0.1.1
+
+-- NOTE: This processor depends on, and thus should be placed before,
+-- the built-in "selector" processor.
 
 local function unicode_code_point_is_chinese(codepoint)
    return (codepoint >= 0x4E00 and codepoint <= 0x9FFF)
@@ -33,22 +36,30 @@ local function processor(key_event, env)
 
    local segment = composition:back()
    local menu = segment.menu
-   local cand_count = menu:candidate_count()
 
+   -- If it is not the first page, simply send 2.
+   local page_size = env.engine.schema.page_size
+   local selected_index = segment.selected_index
+   if selected_index >= page_size then
+      env.engine:process_key(KeyEvent("2"))
+      return kAccepted
+   end
+
+   -- First page: do something more sophisticated.
    local i = 1
-   while i < cand_count do
+   while i < page_size do
       local cand = menu:get_candidate_at(i)
       local cand_text = cand.text
       local codepoint = utf8.codepoint(cand_text, 1)
       if unicode_code_point_is_chinese(codepoint) then
-         context:select(i)
+         env.engine:process_key(KeyEvent(tostring(i+1)))
          return kAccepted
       end
       i = i + 1
    end
 
    -- No good candidates found. Just select the second candidate.
-   context:select(1)
+   env.engine:process_key(KeyEvent("2"))
    return kAccepted
 end
 
