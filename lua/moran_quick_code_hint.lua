31aa54d8a3528df143773dc6ba02d30d1cf81e38
ksqsf <i@ksqsf.moe>
2024-02-17 21:05:25 +0800

    feat: 字詞模式的簡碼提示功能

--- lua/moran_quick_code_hint.lua
+++ lua/moran_quick_code_hint.lua
@@ -6,11 +6,12 @@ function Module.init(env)
       -- The user might have changed it.
       local dict = env.engine.schema.config:get_string("fixed/dictionary")
       env.quick_code_hint_reverse = ReverseLookup(dict)
+      env.quick_code_hint_skip_chars = env.engine.schema.config:get_bool("moran/quick_code_hint_skip_chars") or false
    else
       env.quick_code_hint_reverse = nil
    end
 
-   env.quick_code_indicator = env.engine.schema.config:get_string("moran/quick_code_indicator")
+   env.quick_code_indicator = env.engine.schema.config:get_string("moran/quick_code_indicator") or "⚡"
    env.enable_aux_hint = env.engine.schema.config:get_bool("moran/enable_aux_hint")
 end
 
@@ -34,24 +35,33 @@ function Module.func(translation, env)
    for cand in translation:iter() do
       local gcand = cand:get_genuine()
       local word = gcand.text
-      local all_codes = env.quick_code_hint_reverse:lookup(word)
-      if all_codes then
-         local codes = {}
-         for code in all_codes:gmatch("%S+") do
-            if #code < 4 then
-               table.insert(codes, code)
+      if utf8.len(word) == 1 and env.quick_code_hint_skip_chars then
+         yield(cand)
+      else
+         local all_codes = env.quick_code_hint_reverse:lookup(word)
+         if all_codes then
+            local codes = {}
+            for code in all_codes:gmatch("%S+") do
+               if #code < 4 then
+                  table.insert(codes, code)
+               end
+            end
+            -- the user is using the quick code right now
+            if #codes == 1 and codes[1] == cand.preedit then
+               goto continue
+            end
+            if #codes > 0 then
+               -- do not show two indicators
+               if gcand.comment == indicator then
+                  gcand.comment = gcand.comment .. table.concat(codes, " ")
+               else
+                  gcand.comment = gcand.comment .. indicator .. table.concat(codes, " ")
+               end
             end
          end
-         if #codes > 0 then
-            -- do not show two indicators
-            if gcand.comment == indicator then
-               gcand.comment = gcand.comment .. table.concat(codes, " ")
-            else
-               gcand.comment = gcand.comment .. indicator .. table.concat(codes, " ")
-            end
-         end
+         ::continue::
+         yield(cand)
       end
-      yield(cand)
    end
 end
 
